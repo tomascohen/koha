7754c6fed8cf5b28d2c6ed422f0d0b70a366d845
Tomas Cohen Arazi <tomascohen@theke.io>
2017-01-05 16:41:03 -0300

    Swaggggggggggger

--- Koha/Schema/Result/BorrowerAttributeType.pm
+++ Koha/Schema/Result/BorrowerAttributeType.pm
@@ -165,6 +165,46 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-10-25 20:32:12
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gsPR8PuUUZHFUkr3MIbTpw
 
+__PACKAGE__->add_columns(
+    "+code" => {
+        koha_description => "Unique key used to identify the attribute type"
+    },
+    "+description" =>
+        { koha_description => "Description for the attribute type" },
+    "+repeatable" => {
+        koha_description =>
+            "Defines if a patron can have multiple values for the attribute type"
+    },
+    "+unique_id" => {
+        koha_description => "Defines if the assinged value need to be unique"
+    },
+    "+opac_display" => {
+        koha_description =>
+            "Defines if the attribute is visible to patrons on their account in the OPAC or not"
+    },
+    "+opac_editable" => {
+        koha_description =>
+            "Defines if the attribute is editable by patrons on their account in the OPAC or not"
+    },
+    "+staff_searchable" => {
+        koha_description =>
+            "Defines if the attribute is searchable via the patron search in the staff client"
+    },
+    "+authorised_value_category" => {
+        koha_description =>
+            "Foreign key from authorised_values that links this attribute to an authorized value category"
+    },
+    "+display_checkout" => {
+        koha_description =>
+            "Defines if the attribute  is displayed in checkout screens"
+    },
+    "+category_code" => {
+        koha_description =>
+            "Defines a constraint on a patron category for the attribute type"
+    },
+    "+class" =>
+        { koha_description => "Defines a class for grouping attributes" }
+);
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
