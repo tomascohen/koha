dcd6974200fa3583d13bd69b2d84816b1ed79126
Tomas Cohen Arazi <tomascohen@theke.io>
2017-12-05 16:35:06 -0300

    Bug 16330: (QA followup) Add missing POD for exceptions
    This patch adds POD to the exceptions files introduced by this patchset.
    
    Note: the qa scripts give false positives on Exceptions. But the patchset
    was missing them anyway.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Exceptions/Category.pm
+++ Koha/Exceptions/Category.pm
@@ -1,5 +1,20 @@
 package Koha::Exceptions::Category;
 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 use Modern::Perl;
 
 use Exception::Class (
@@ -13,5 +28,21 @@ use Exception::Class (
         fields => ["categorycode"],
     },
 );
+
+=head1 NAME
+
+Koha::Exceptions::Category - Base class for patron categories exceptions
+
+=head1 Exceptions
+
+=head2 Koha::Exceptions::Category
+
+Generic patron category exception
+
+=head2 Koha::Exceptions::Category::CategorycodeNotFound
+
+Exception to be used when the required patron category code is not found.
+
+=cut
 
 1;
--- Koha/Exceptions/Library.pm
+++ Koha/Exceptions/Library.pm
@@ -1,5 +1,20 @@
 package Koha::Exceptions::Library;
 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 use Modern::Perl;
 
 use Exception::Class (
@@ -13,5 +28,21 @@ use Exception::Class (
         fields => ["branchcode"],
     },
 );
+
+=head1 NAME
+
+Koha::Exceptions::Library - Base class for libraries exceptions
+
+=head1 Exceptions
+
+=head2 Koha::Exceptions::Library
+
+Generic library exception
+
+=head2 Koha::Exceptions::Library::BranchcodeNotFound
+
+Exception to be used when the required library code is not found.
+
+=cut
 
 1;
